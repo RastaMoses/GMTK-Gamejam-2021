8aba9f009b0b1e77acf11afd383d4bd27c73b243
Tiffin007 <s-leischmidt@web.de>
2021-06-12 00:42:39 +0200

    First 3 level templates added

---
+++ Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public GameObject[] objects;
+    void Start()
+    {
+    }
+
+    private void Update()
+    {
+        int rand = Random.Range(0, objects.Length);
+        Instantiate(objects[rand], transform.position, Quaternion.identity);
+    }
+}
