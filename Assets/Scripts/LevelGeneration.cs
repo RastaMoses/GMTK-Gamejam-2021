a3db32b2a2539bd518bfcde31b6572443bb00f43
Tiffin007 <s-leischmidt@web.de>
2021-06-12 01:28:22 +0200

    spawning gameobjects as children

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGeneration : MonoBehaviour
-{
+{   
     public GameObject[] objects;
-    void Start()
-    {
-    }
-
+    public float elapsedTime = 0.0f;
+    private float secondsBetweenSpawns = 2f;  
     private void Update()
     {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >secondsBetweenSpawns)
+        {
+            elapsedTime = 0;
+            SpawnBlocks();
+        } 
+    }
+    private void SpawnBlocks()
+    {
         int rand = Random.Range(0, objects.Length);
-        Instantiate(objects[rand], transform.position, Quaternion.identity);
+        GameObject go = Instantiate(objects[rand], transform.position, Quaternion.identity);
+        go.transform.SetParent(gameObject.transform);
     }
 }
