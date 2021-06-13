3c15800db47a564a65cf09cdc8e55cab6b1e5bf8
Leon <leon@nyoucomer.de>
2021-06-13 15:47:36 +0200

    Added Eject

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -8,10 +8,17 @@ public class CharacterController : MonoBehaviour
     [SerializeField] float rotateSpeed;
     [SerializeField] float boostForce = 5f;
     [SerializeField] float maxSpeed = 100f;
+    [SerializeField] float maxMass = 100f;
+
+    [SerializeField] float ejectForce = 200f;
+    [SerializeField] float ejectRadius = 5f;
+    [SerializeField] float ejectCooldown = 10f;
 
 
     //State
+    bool onCooldown = false;
     Vector3 mouseWorldPosition;
+    bool tooHeavy = false;
 
     //cached comp
     Rigidbody rb;
@@ -22,8 +29,15 @@ public class CharacterController : MonoBehaviour
     }
     void FixedUpdate()
     {
+        if (rb.mass > maxMass)
+        {
+            tooHeavy = true;
+        }
+        else
+        {
+            tooHeavy = false;
+        }
         mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 100f);
-        Debug.Log(rb.velocity.magnitude);
         if (rb.velocity.magnitude > 100)
         {
             rb.velocity = Vector3.zero;
@@ -37,10 +51,9 @@ public class CharacterController : MonoBehaviour
         transform.rotation = Quaternion.RotateTowards(transform.rotation,Quaternion.Euler(new Vector3(0f, 0f, angle)),rotateSpeed);
         
 
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Boost();
-        }
+        
+
+        
         
     }
 
@@ -52,7 +65,18 @@ public class CharacterController : MonoBehaviour
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!onCooldown)
+            {
+                Eject();
+            }
+
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Boost();
+        }
     }
 
     void Boost()
@@ -61,10 +85,46 @@ public class CharacterController : MonoBehaviour
         {
             return;
         }
-        rb.AddRelativeForce(Vector3.left * boostForce * Time.fixedDeltaTime);
+        rb.AddRelativeForce(Vector3.left * boostForce * Time.deltaTime);
     }
 
-    
+    public bool GetHeavy()
+    {
+        return tooHeavy;
+    }
+
+    void Eject()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.GetComponent<Sticky>())
+            {
+                child.GetComponent<Sticky>().Unstick();
+            }
+        }
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, ejectRadius);
+        foreach (var hitCollider in hitColliders)
+        {
+
+            if (hitCollider.GetComponent<Sticky>())
+            {
+                hitCollider.GetComponent<Sticky>().Unstick();
+            }
+            if (hitCollider.GetComponent<Rigidbody>() && hitCollider.GetComponent<CharacterController>() == false)
+            {
+                hitCollider.GetComponent<Rigidbody>().AddExplosionForce(ejectForce, transform.position, 0);
+            }
+
+        }
+        StartCoroutine(ResetEject());
+    }
+
+    IEnumerator ResetEject()
+    {
+        onCooldown = true;
+        yield return new WaitForSeconds(ejectCooldown);
+        onCooldown = false;
+    }
 
 
 
